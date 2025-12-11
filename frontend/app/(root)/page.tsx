b67877d6d99bf3318b880ba66faf280968fa7df9
Pixelated-Sage <thepixelatedsage@gmail.com>
2025-12-12 01:12:59 +0530

    git package issue resolved

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="text-4xl font-bold flex justify-center text-center">Oxlate</h1>
+    <h1 className="font-[Orbitron] text-4xl font-bold flex justify-center text-center">Oxlate</h1>
     </>
   );
 }
